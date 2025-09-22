2b981a2a7bad45356235cb2efa70e86efc77b8f4
Junjian Ni <junjian.ni@syngenta.com>
2025-09-21 22:06:23 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+  const [word, setWord] = React.useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    // Handle search logic here
+    console.log(word);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Image Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
